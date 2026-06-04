1954b3a9321e00e0159311b2e7ee436b5059e078
samyak1901 <samyak1901@gmail.com>
2026-06-04 13:36:34 +1000

    ci: deploy static site to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? "/website" : "",
+  assetPrefix: isGithubPages ? "/website/" : "",
 };
 
 export default nextConfig;
